513f185c8cd790a4e538cefb8c561d5153331462
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-03 19:14:12 +0200

    WIP: try to get infrared working

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@
 
 use arduino_hal::{delay_ms, prelude::*};
 use panic_halt as _;
+use ufmt::uwriteln;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
+    // let timer2 = arduino_hal::simple_pwm::Timer2Pwm::new(
+    //     dp.TC2,
+    //     arduino_hal::simple_pwm::Prescaler::Prescale8,
+    // );
+
+    let mut ir_led = pins.d3.into_output();
+
     loop {
-        ufmt::uwriteln!(&mut serial, "hello").unwrap_infallible();
-        delay_ms(1000);
+        // send the power command over and over
     }
 }
