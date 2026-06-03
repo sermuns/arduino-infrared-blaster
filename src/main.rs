ff67c1ae22a4e4e62ab80813f9b9d914a519e0bc
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-03 22:31:59 +0200

    WIP: sending NEC frames, not quite working, i think

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::{delay_ms, prelude::*};
+use arduino_hal::{delay_ms, delay_us, prelude::*};
 use panic_halt as _;
 use ufmt::uwriteln;
 
-fn enable_carrier(tc2: &arduino_hal::pac::TC2) {
+const TIME_UNIT_US: u32 = 562;
+
+fn mark(tc2: &arduino_hal::pac::TC2, us: u32) {
     tc2.tccr2a().modify(|_, w| {
         w.com2b().match_toggle();
         w
     });
+    delay_us(us);
 }
 
-fn disable_carrier(tc2: &arduino_hal::pac::TC2) {
+fn space(tc2: &arduino_hal::pac::TC2, us: u32) {
     tc2.tccr2a().modify(|_, w| {
         w.com2b().disconnected();
         w
     });
+    delay_us(us);
+}
+
+fn send_bit(tc2: &arduino_hal::pac::TC2, bit: bool) {
+    mark(tc2, TIME_UNIT_US);
+
+    if bit {
+        space(tc2, TIME_UNIT_US * 3);
+    } else {
+        space(tc2, TIME_UNIT_US);
+    }
+}
+
+fn send_nec_frame(tc2: &arduino_hal::pac::TC2, addr: u32, cmd: u32) {
+    mark(tc2, TIME_UNIT_US * 16);
+    space(tc2, TIME_UNIT_US * 8);
+
+    for i in 0..32 {
+        let bit = (addr >> i) & 1;
+        send_bit(tc2, bit != 0);
+    }
+
+    for i in 0..32 {
+        let bit = (cmd >> i) & 1;
+        send_bit(tc2, bit != 0);
+    }
+
+    mark(tc2, TIME_UNIT_US);
 }
 
 #[arduino_hal::entry]
@@ -26,7 +57,7 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     let tc2 = dp.TC2;
-    let _d3 = pins.d3.into_output();
+    let mut d3 = pins.d3.into_output();
 
     tc2.tccr2a().write(|w| {
         w.wgm2().ctc();
@@ -42,12 +73,8 @@ fn main() -> ! {
     tc2.ocr2a().write(|w| unsafe { w.bits(210) });
 
     loop {
-        enable_carrier(&tc2);
-        uwriteln!(serial, "Carrier enabled").unwrap_infallible();
-        delay_ms(1000);
-
-        disable_carrier(&tc2);
-        uwriteln!(serial, "Carrier disabled").unwrap_infallible();
+        uwriteln!(serial, "sending poweroff!").unwrap_infallible();
+        send_nec_frame(&tc2, 0x86FF0000, 0x1BE40000);
         delay_ms(1000);
     }
 }
