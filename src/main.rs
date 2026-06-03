674f4d3adb4295b56c89dbf0dd14c4321db83a89
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-03 18:08:48 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use arduino_hal::{delay_ms, prelude::*};
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    loop {
+        ufmt::uwriteln!(&mut serial, "hello").unwrap_infallible();
+        delay_ms(1000);
+    }
+}
